26e3b6d4a3be32a59849d89abe5c7408cffd1be1
NobleMarten <mkir2006@gmail.com>
2026-01-18 13:49:50 +0300

    start server upd 1.1

--- cmd/todo-api/main.go
+++ cmd/todo-api/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"todo/internal/service"
+	"todo/internal/storage"
+	"todo/internal/transport"
+)
+
+func main() {
+	store := storage.NewFileStorage("data/tasks.json")
+
+	svc := service.NewTaskService(store)
+
+	h := transport.NewHandler(svc)
+
+	http.HandleFunc("/todos", h.GetTodos) // Регистрируем обработчик для пути /todos.
+
+	// Запускаем HTTP-сервер на порту 8080.
+	log.Println("API server started: http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil)) // Логируем ошибку, если сервер не смог запуститься.
+
+}
--- internal/transport/handler.go
+++ internal/transport/handler.go
@@ -1,1 +1,39 @@
 package transport
+
+import (
+	"encoding/json"
+	"net/http"
+	"todo/internal/service"
+)
+
+// Handler представляет собой структуру, которая содержит ссылку на сервис TaskService.
+type Handler struct {
+	svc *service.TaskService // Поле svc хранит указатель на экземпляр TaskService.
+}
+
+// NewHandler создает новый экземпляр Handler, принимая указатель на TaskService.
+func NewHandler(svc *service.TaskService) *Handler {
+	// Возвращает указатель на новый экземпляр Handler, инициализированный переданным сервисом.
+	return &Handler{svc: svc} // *Handler означает, что функция возвращает указатель на Handler.
+}
+
+func (h *Handler) GetTodos(w http.ResponseWriter, r *http.Request) {
+	// Логика обработки запроса на получение списка задач.
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tasks, err := h.svc.List()
+	if err != nil {
+		http.Error(w, "Failed to load tasks", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json") // Устанавливаем заголовок Content-Type в ответе.
+
+	if err := json.NewEncoder(w).Encode(tasks); err != nil { // Кодируем задачи в JSON и записываем их в ответ.
+		http.Error(w, "Failed to encode tasks", http.StatusInternalServerError)
+		return
+	}
+}
